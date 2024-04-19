579005800751d18cd2e1f4dcf80671c01a22ef5c
Julian Vilfort <j.vilfort01@gmail.com>
2024-04-19 18:32:55 -0400

    established mysql connection and some post routes

--- server/index.js
+++ server/index.js
@@ -16,6 +16,42 @@ const con = mysql.createConnection(
     }
 )
 
-app.listen(3001, () => {
+app.post('/register', (req,res) => {
+    const email = req.body.email;
+    const username = req.body.username;
+    const password = req.body.password;
+
+    con.query('INSERT INTO users (email, username, password) VALUES(?,?,?)', [email,username,password],
+        (err, result) => {
+            if(result){
+                res.send(result);
+            }else{
+                res.send({message: 'ENTER CORRECT ASKED DETAILS!'})
+            }
+        }
+    )
+})
+
+app.post('/login', (req,res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    con.query('SELECT * FROM users WHERE username = ? AND password = ?', [username,password],
+        (err, result) => {
+            if(result){
+                res.send(result);
+            }else{
+                res.send({message: 'ENTER CORRECT ASKED DETAILS!'})
+            }
+        }
+    )
+})
+
+app.post('/', (req,res) => {
+    console.log('fuh')
+    res.send('yeo')
+})
+
+app.listen(3003, () => {
     console.log('running backend server');
 })
