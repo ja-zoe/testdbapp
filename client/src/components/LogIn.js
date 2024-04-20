5b392cc1f1bcb31e381702b6a0dd6b922000b2ee
Julian Vilfort <j.vilfort01@gmail.com>
2024-04-20 08:50:48 -0400

    added register and login functions

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -7,6 +7,10 @@ const LogIn = () => {
     const [username,setUsername] = useState('');
     const [password,setPassword] = useState('');
     const [loginStatus,setloginStatus] = useState('');
+    
+    const logIn = () => {
+
+    }
 
     return (
         <div className={styles.container}>
@@ -14,13 +18,13 @@ const LogIn = () => {
             <form className={styles.form}>
                 <label className={styles.label}>
                     <span className={styles.span}> Username/Email </span>
-                    <input type='text' onChange={(e) => {setUsername(e)}}/>
+                    <input type='text' onChange={(e) => {setUsername(e.target.value)}}/>
                 </label>
                 <label className={styles.label}>
                     <span className={styles.span}> Password </span>
-                    <input type='password' onChange={(e) => {setPassword(e)}}/>
+                    <input type='password' onChange={(e) => {setPassword(e.target.value)}}/>
                 </label>
-                <button> Log In </button>
+                <button onClick={logIn}> Log In </button>
             </form>
             <Link to='/sign-up'> Need an Account? </Link>
             <Link to='/'> Home </Link>
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -8,23 +8,27 @@ const SignUp = () => {
     const [password,setPassword] = useState('');
     const [registerStatus,setRegisterStatus] = useState('');
 
+    const register = () => {
+
+    }
+
     return (
         <div className={styles.container}>
             <h1 className={styles.text}> Sign Up </h1>
             <form className={styles.form}>
                 <label className={styles.label}>
                     <span className={styles.span}> Username </span>
-                    <input type='text'onChange={(e) => {setUsername(e)}}/>
+                    <input type='text'onChange={(e) => {setUsername(e.target.value)}}/>
                 </label>
                 <label className={styles.label}>
                     <span className={styles.span}> Email </span>
-                    <input type='email' onChange={(e) => {setEmail(e)}}/>
+                    <input type='email' onChange={(e) => {setEmail(e.target.value)}}/>
                 </label>
                 <label className={styles.label}>
                     <span className={styles.span}> Password </span>
-                    <input type='password' onChange={(e) => {setPassword(e)}}/>
+                    <input type='password' onChange={(e) => {setPassword(e.target.value)}}/>
                 </label>
-                <button> Sign Up </button>
+                <button onClick={register}> Sign Up </button>
             </form>
             <Link to='/login' className={styles.link}> Alredy Have an Account? </Link>
             <Link to='/'> Home </Link>
