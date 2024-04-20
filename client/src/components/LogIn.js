00eaa0e99b3086f02dc71358516932731ad607bd
Julian Vilfort <j.vilfort01@gmail.com>
2024-04-20 08:47:37 -0400

    added useState variables for registration and log in

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import { React, useState } from 'react';
 import styles from '../styles/LogIn.module.css';
 import { Link } from 'react-router-dom';
 
 const LogIn = () => {
+    const [email,setEmail] = useState('');
+    const [username,setUsername] = useState('');
+    const [password,setPassword] = useState('');
+    const [loginStatus,setloginStatus] = useState('');
+
     return (
         <div className={styles.container}>
             <h1 className={styles.text}> Log In </h1>
             <form className={styles.form}>
                 <label className={styles.label}>
                     <span className={styles.span}> Username/Email </span>
-                    <input type='text'></input>
+                    <input type='text' onChange={(e) => {setUsername(e)}}/>
                 </label>
                 <label className={styles.label}>
                     <span className={styles.span}> Password </span>
-                    <input type='password'></input>
+                    <input type='password' onChange={(e) => {setPassword(e)}}/>
                 </label>
                 <button> Log In </button>
             </form>
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,23 +1,28 @@
-import React from 'react';
+import { React, useState } from 'react';
 import styles from '../styles/SignUp.module.css'
 import { Link } from 'react-router-dom';
 
 const SignUp = () => {
+    const [email,setEmail] = useState('');
+    const [username,setUsername] = useState('');
+    const [password,setPassword] = useState('');
+    const [registerStatus,setRegisterStatus] = useState('');
+
     return (
         <div className={styles.container}>
             <h1 className={styles.text}> Sign Up </h1>
             <form className={styles.form}>
                 <label className={styles.label}>
                     <span className={styles.span}> Username </span>
-                    <input type='text'/>
+                    <input type='text'onChange={(e) => {setUsername(e)}}/>
                 </label>
                 <label className={styles.label}>
                     <span className={styles.span}> Email </span>
-                    <input type='email'/>
+                    <input type='email' onChange={(e) => {setEmail(e)}}/>
                 </label>
                 <label className={styles.label}>
                     <span className={styles.span}> Password </span>
-                    <input type='password'/>
+                    <input type='password' onChange={(e) => {setPassword(e)}}/>
                 </label>
                 <button> Sign Up </button>
             </form>
